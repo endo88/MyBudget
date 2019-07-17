5285925a2b1aeb217b5fd067e558d49ea91dadd5
Dawid Grzegorski <dawg@hoistfinance.onmicrosoft.com>
2019-07-17 12:23:25 +0200

    Added CryptoCurrency

---
+++ MyBudget/Controllers/CryptoController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyBudget.Models;
+using Newtonsoft.Json;
+using unirest_net.http;
+
+namespace MyBudget.Controllers
+{
+    public class CryptoController : Controller
+    {
+        private List<CryptoCurrency> currencies;
+
+        private List<string> coinIDs;
+
+        public CryptoController()
+        {
+            currencies = new List<CryptoCurrency>();
+            coinIDs = new List<string>
+            {
+                "btc",
+                "ltc",
+                "eth",
+                "xrp",
+                "bch"
+            };
+        }
+
+        public ViewResult List()
+        {
+            foreach (var Id in coinIDs)
+            {
+                var response = GetCurrencyRatesByCoinId(Id);
+
+                ParseBody(response.Body);
+            }
+
+            return View("List",currencies);
+        }
+
+        private void ParseBody(string responseBody)
+        {
+            var body = JsonConvert.DeserializeObject<CryptoCurrency>(responseBody);
+
+            body.Prices = body.Prices.Where(p => p.CurrencyID == "USD").ToList();
+
+            currencies.Add(body);
+        }
+
+        private HttpResponse<string> GetCurrencyRatesByCoinId(string coinId)
+        {
+            var url = $"https://bravenewcoin-v1.p.rapidapi.com/prices?coin={coinId}";
+            HttpResponse<string> response = Unirest.get(url)
+                .header("X-RapidAPI-Host", "bravenewcoin-v1.p.rapidapi.com")
+                .header("X-RapidAPI-Key", "4114f32b36mshed4ee80973c502fp17b8cejsna4d766f940ef")
+                .asJson<string>();
+
+            return response;
+        }
+    }
+}
---
+++ MyBudget/Models/CryptoCurrency.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MyBudget.Models
+{
+    public class CryptoCurrency
+    {
+        [JsonProperty(PropertyName = "Coin_Id")]
+        public string CoinID { get; set; }
+        public List<ExchangeDetail> Prices { get; set; }
+    }
+
+    public class ExchangeDetail
+    {
+        [JsonProperty(PropertyName = "Id_currency")]
+        public string CurrencyID { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+    }
+}
